37be55ef14e750b08de275833199b67314f80866
rodrigoschlichta <rodrigo_schlichta@hotmail.com>
2023-01-17 16:40:19 -0300

    DTo and Service

--- backend/src/main/java/com/camfel/gamesurvey/repositories/RecordRepository.java
+++ backend/src/main/java/com/camfel/gamesurvey/repositories/RecordRepository.java
@@ -1,9 +1,11 @@
 package com.camfel.gamesurvey.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.camfel.gamesurvey.entities.Record;
 
+@Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
 }
